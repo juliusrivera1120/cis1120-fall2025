6bacad84ea781ce3e149b6f61599e554064fc09f
jschanker <jschanker@molloy.edu>
2025-09-03 14:20:57 -0400

    First Lab upload

---
+++ src/pages/Resume.js
@@ -0,0 +1,36 @@
+import ResumeItem from "../components/ResumeItem";
+
+export default function Resume() {
+  return (
+    <>
+      Alvaro Montoro Senior Software Engineer with strong experience creating
+      high-quality, scalable, and high-performance web applications seeking
+      front-end and full stack opportunities. EXPERIENCE FILTERS athenahealth
+      Healthcare IT Member of Technical Staff 2017 - 2019 Developed widgets
+      using React and Less to display patient information. Created design
+      specifications, participated in design and code reviews. Worked on the
+      design and initial development of a new platform to import medical
+      documents. Developed a Kotlin micro-service to generate XML documents
+      based on a government-compliant architecture (CCDA). General Motors
+      Cadillac.Com Team Web Developer 2015 - 2017 Developed new components for
+      the award-winning website Cadillac.com and its mobile version. Worked with
+      stakeholders to refine and negotiate product requirement specifications,
+      functional specifications, and product vision and direction. Designed a
+      web component that reduced page load speed by over 15%, improving SEO and
+      accessibility, and saving authoring expenses. Organized and participated
+      in events representing GM at volunteering opportunities and competitions.
+      MicroAssist Information Technology and Services PHP Developer 2014 - 2015
+      Designed and developed different web projects with a focus on
+      accessibility, including new features and website enhancements. Created
+      and integrated an automated mail system with 3rd party APIs. Developed and
+      collaborated in the translation project of the ITTD website for the Texas
+      Department of Public Safety. Hewlett-Packard Content & Digital Asset
+      Management IT Engineer 2011 - 2014 Developed two internal content
+      management systems (related to HP’s products documentation) with over
+      three million monthly visitors. Coordinated efforts with an overseas team,
+      applying Agile methodologies to ensure on-time, on-spec project delivery.
+      Improved different batch jobs performance, reducing execution time by over
+      50%, and eliminating downtime.
+    </>
+  );
+}
